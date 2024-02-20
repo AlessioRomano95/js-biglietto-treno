c5226952f97aea1f61c95fdb2f1e04715764dc06
AlessioRomano95 <romanalex400@gmail.com>
2024-02-20 18:40:39 +0100

    [ADD] prezzo treno con sconti

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+console.log('prezzo treno')
+
+const prezzo = document.getElementById('prezzo')
+
+const userKm = parseInt(prompt('Quanti chilometri vuoi fare?'))
+
+const userAge = parseInt(prompt('Quanti anni hai?'))
+
+const fixedPrice = 0.21
+
+let totalPrice = fixedPrice * userKm
+
+let discountPrice = .20
+
+let message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+
+if (userAge < 18 ){
+    totalPrice += - (totalPrice * discountPrice)
+    message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+} else if (userAge > 65) {
+    discountPrice = .40
+    totalPrice += - (totalPrice * discountPrice)
+    message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+}
+
+prezzo.innerText = message
